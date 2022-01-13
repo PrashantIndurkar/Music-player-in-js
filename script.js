7b59c94de8c907be0e4fa4d7f2710c7ff0027033
PrashantIndurkar <prashantri2000@gmail.com>
2022-01-13 23:58:13 +0530

    added js and function to load song

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const progress = document.getElementById("progress");
 const progressContainer = document.getElementById("progress_container");
 const title = document.getElementById("title");
 const cover = document.getElementById("cover");
+
+// songs title
+const songs = ["hey", "summer", "ukulele"];
+
+// Keep Track of Song
+let songIndex = [2];
+
+// initially load song details into dom
+loadSong(songs[songIndex]);
+
+// update song detail
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
